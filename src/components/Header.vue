ff56b277832a452059e7753b8791a45fe2e0075b
= <=>
2021-05-18 11:31:49 +0300

    Refactored files` position and addead Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper pink lighten-1r">
+          <a href="#!" class="brand-logo right" @click.prevent>Logo</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"
+            ><i class="material-icons">menu</i></a
+          >
+          <ul class="hide-on-med-and-down left navbar-tabs">
+            <router-link
+              v-for="link of links"
+              :key="link.to"
+              :to="link.to"
+              :exact="link.exact || false"
+              tag="li"
+              active-class="active"
+              class="waves-effect pointer"
+            >
+              <a href="#">{{ link.text }}</a>
+            </router-link>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <ul class="sidenav white" id="mobile-demo">
+      <li class="center-align">
+        <i class="material-icons">account_circle</i>
+        <br />
+        username
+      </li>
+      <li><div class="divider"></div></li>
+
+      <template v-for="link of links">
+        <router-link
+          :key="link.to"
+          :to="link.to"
+          :exact="link.exact || false"
+          tag="li"
+          active-class="active"
+          class="waves-effect pointer"
+        >
+          <a href="#">{{ link.text }}</a>
+        </router-link>
+
+        <li :key="link.to"><div class="divider"></div></li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<style lang="less" scoped>
+#mobile-demo {
+  i {
+    font-size: 80px;
+  }
+}
+
+.navbar-tabs {
+  margin-left: 15vw;
+
+  & li {
+    padding-right: 20px;
+    padding-left: 20px;
+    cursor: pointer;
+
+    & a:hover {
+      background: none;
+    }
+  }
+}
+
+.brand-logo {
+  margin-right: 15vw;
+}
+
+.sidenav {
+  li {
+    display: block;
+  }
+
+  .divider {
+    margin: none;
+  }
+}
+</style>
+
+<script>
+import M from "materialize-css";
+
+export default {
+  data: () => ({
+    sidenavInstances: [],
+    links: [
+      {
+        to: "/",
+        text: "Головна",
+        exact: true,
+      },
+      {
+        to: "/basket",
+        text: "Корзина",
+      },
+      {
+        to: "/history",
+        text: "Моя історія покупок",
+      },
+    ],
+  }),
+  mounted() {
+    const elems = document.querySelectorAll(".sidenav");
+    this.sidenavInstances = M.Sidenav.init(elems);
+  },
+  beforeDestroy() {
+    this.sidenavInstances.forEach((e) => e.destroy());
+  },
+};
+</script>
